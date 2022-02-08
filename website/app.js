d52c1ca34fd85710542c726e4e3acf529d4d9949
Olayinka Ola <mimoyinka@gmail.com>
2022-02-08 17:13:21 +0100

    Chore: Code Reformat for better readability

--- website/app.js
+++ website/app.js
@@ -7,6 +7,33 @@ let d = new Date();
 // Adds 1 to month because its zero based.
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+
+function main(){
+    let elem = document.getElementById("generate");
+    elem.addEventListener('click', handleGenerateClick);
+}
+
+window.addEventListener('load', main);
+
+async function handleGenerateClick(){
+    let zip = document.getElementById("zip").value;
+    let feelings = document.getElementById("feelings").value;
+    let data = {
+        date: newDate,
+        content: feelings
+    };
+    try {
+        execFetch(baseUrl,zip,openWeatherKey)
+            .then((temperature) => {
+                return postData("/project", {...data, temperature})
+            }, handleError)
+            .then((res) => fetch('/project')).then(res => res.json())
+            .then((data) => updateUI(data));
+    } catch (e) {
+        console.log(e)
+    }
+}
+
 async function execFetch(baseUrl, zip, apiKey) {
         // 94040
        return fetch(`${baseUrl}/weather?zip=${zip},us&appid=${apiKey}`)
@@ -32,37 +59,12 @@ async function postData(path="/project", data){
 }
 
 
-function main(){
-    let elem = document.getElementById("generate");
-    elem.addEventListener('click', handleGenerateClick);
-}
-
-window.addEventListener('load', main);
-
 async function updateUI(data){
     document.getElementById("date").innerHTML = `<span>${data.date}</span>`;
     document.getElementById("temp").innerHTML = `<span>${data.temperature}</span>`;
     document.getElementById("content").innerHTML = `<span>${data.content}</span>`;
 }
 
-async function handleGenerateClick(){
-    let zip = document.getElementById("zip").value;
-    let feelings = document.getElementById("feelings").value;
-    let data = {
-        date: newDate,
-        content: feelings
-    };
-    try {
-        execFetch(baseUrl,zip,openWeatherKey)
-            .then((temperature) => {
-                return postData("/project", {...data, temperature})
-            }, handleError)
-            .then((res) => fetch('/project')).then(res => res.json())
-            .then((data) => updateUI(data));
-    } catch (e) {
-        console.log(e)
-    }
-}
 
 const handleError = (e) => {
     return Promise.reject(e)
